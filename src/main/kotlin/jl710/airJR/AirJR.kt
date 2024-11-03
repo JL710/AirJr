4a129b51eaa10f6a9e03ac3385519ab45c09e65b
JL710 <76447362+JL710@users.noreply.github.com>
2024-11-03 14:49:19 +0100

    remove unused imports

--- src/main/kotlin/jl710/airJR/AirJR.kt
+++ src/main/kotlin/jl710/airJR/AirJR.kt
@@ -1,10 +1,8 @@
 package jl710.airJR
 
-import org.bukkit.Location
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 interface SubPlugin: CommandExecutor {
