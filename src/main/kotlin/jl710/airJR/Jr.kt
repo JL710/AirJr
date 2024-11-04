f18ec7a0de76562aa19cc67101375e80c0e33f2f
JL710 <76447362+JL710@users.noreply.github.com>
2024-11-04 20:29:58 +0100

    improve drastically how jump and runs are generated
    performance and playability

--- src/main/kotlin/jl710/airJR/Jr.kt
+++ src/main/kotlin/jl710/airJR/Jr.kt
@@ -7,6 +7,11 @@ import org.bukkit.Location
 import org.bukkit.util.Vector
 
 data class Jr(val id: Int, val location1: Location, val location2: Location, val name: String) {
+
+    companion object {
+        private val possibleBlocks = nextBlockOffsets()
+    }
+
     init {
         if (location1.world.uid != location2.world.uid) {
             throw IllegalArgumentException("location1 and location2 have different worlds!")
@@ -18,27 +23,27 @@ data class Jr(val id: Int, val location1: Location, val location2: Location, val
             return blocks.first().clone().setX(blocks.first().x + 3)
         }
 
-        val newBlock = blocks.last().clone()
-        while (!inBounds(newBlock) ||
-                newBlock.y in
-                        blocks
-                                .map { x -> x.y }
-                                .toSet() || // check if blocks are under / over each other
-                newBlock.distance(blocks.last()) < 1 ||
-                newBlock.distance(blocks.last()) >= 5 ||
-                newBlock.distance(blocks.first()) <
-                        5 || // the blocks need to be somewhat far away from each other and not
-                // stacked up
-                minAngle(
-                        blocks.last().clone().subtract(newBlock),
-                        blocks.dropLast(1).last().clone().subtract(newBlock)
-                ) < 0.26) { // 0.27 = 15°
-            newBlock.x = blocks.last().x + randomInt(-4, 4).toDouble()
-            newBlock.y = blocks.last().y + randomInt(-1, 1).toDouble()
-            newBlock.z = blocks.last().z + randomInt(-4, 4).toDouble()
+        for (offset in possibleBlocks.shuffled()) {
+            val newBlock = blocks.last().clone().add(offset)
+            if (!inBounds(newBlock)) {
+                continue
+            }
+            if (blocks.find { x -> x.x == newBlock.x && x.z == newBlock.z } != null) {
+                continue
+            }
+            if (minAngle(
+                            blocks.last().clone().subtract(newBlock),
+                            blocks.dropLast(1).last().clone().subtract(newBlock)
+                    ) < 0.26
+            ) {
+                continue
+            }
+            if (blocks.dropLast(1).last().distance(newBlock) < 3) {
+                continue
+            }
+            return newBlock
         }
-
-        return newBlock
+        throw RuntimeException("Could not find a good next block")
     }
 
     private fun minAngle(vec1: Vector, vec2: Vector): Float {
@@ -83,6 +88,51 @@ data class Jr(val id: Int, val location1: Location, val location2: Location, val
 
 private fun randomInt(start: Int, end: Int): Int {
     val rangeLength = abs(start - end)
-    val randomNumber = min(start, end) + Random.nextInt(rangeLength)
+    val randomNumber =
+            min(start, end) +
+                    Random.nextInt(rangeLength + 1) // +1 because the end should be included
     return randomNumber
+}
+
+private fun nextBlockOffsets(): MutableList<Vector> {
+    val offsets = mutableListOf<Vector>()
+
+    val single_corner =
+            listOf(
+                    // Straight lines
+                    Vector(2, 0, 0),
+                    Vector(3, 0, 0),
+                    Vector(4, 0, 0),
+                    Vector(2, 1, 0),
+                    Vector(3, 1, 0),
+                    Vector(4, 1, 0),
+                    Vector(2, -1, 0),
+                    Vector(3, -1, 0),
+                    Vector(4, -1, 0),
+                    Vector(2, -2, 0),
+                    Vector(3, -2, 0),
+                    Vector(4, -2, 0),
+                    // diagonal
+                    Vector(2, 0, 2),
+                    Vector(2, 1, 2),
+                    Vector(2, -1, 2),
+                    Vector(2, -2, 2),
+                    Vector(3, 0, 2),
+                    // other diagonals
+                    Vector(3, 1, 2),
+                    Vector(3, -1, 2),
+                    Vector(3, -2, 2),
+                    Vector(2, 0, 3),
+                    Vector(2, 1, 3),
+                    Vector(2, -1, 3),
+                    Vector(2, -2, 3),
+            )
+    for (v in single_corner) {
+        offsets.add(Vector(v.x * -1, v.y, v.z))
+        offsets.add(Vector(v.z, v.y, v.x))
+        offsets.add(Vector(v.z, v.y, v.x * -1))
+    }
+    offsets.addAll(single_corner)
+
+    return offsets
 }
